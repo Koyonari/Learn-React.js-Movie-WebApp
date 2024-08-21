265c1394ec395ff94a3bb845b899e8845e4515bb
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-22 01:20:03 +0800

    feat: add api and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+//b690ae75
+const API_URL = "http://www.omdbapi.com/?apikey=b690ae75";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
   return <h1 className="font-bold text-3xl text-cyan-500">App</h1>;
 };
 
