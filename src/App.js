14b50a2c6ac6d55b1082d068e49c25dd170c81c3
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-21 15:15:59 +0800

    feat: tailwind and react setup, functions added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={"John"} age={"10"} />
+      <Person name={"Janet"} age={20 + 10} />
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold font-mono text-cyan-300">Name: John</h1>
+      <h2 className="font-bold font-mono text-2xl">Age: 30</h2>
     </div>
   );
 }
