a28285ef5ddbc4f454e29c8e7d5d7bf5c10d181b
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-22 02:02:11 +0800

    feat: add search and display

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Join me on my journey to learn React.js with TailwindCSS
+# Join me on my journey to learn React.js
 
 ## Purpose
 
-This repository is dedicated to learning the React.js framework along with TailwindCSS via project-based learning with my background of pure HTML, CSS and Javascript. I aim to learn the ins-and-outs of React.js and TailwindCSS, especially the fundamentals.
+This repository is dedicated to learning the React.js framework along with TailwindCSS via project-based learning with my background of pure HTML, CSS and Javascript. I aim to learn the ins-and-outs of React.js, especially the fundamentals.
 
 `My notes: https://fluorescent-frame-930.notion.site/React-js-with-TailwindCSS-135bc12c4a29421fbf20d12f5ade4ff9?pvs=4`
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-//b690ae75
+// Api key
 const API_URL = "http://www.omdbapi.com/?apikey=b690ae75";
 
-const movie1 = {
-  Title: "Batman Begins",
-  Year: "2005",
-  imdbID: "tt0372784",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-};
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -26,40 +22,36 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1 className="font-bold text-5xl">MovieLand</h1>
 
+      {/* User input for searching movies */}
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+
+        {/* Search icon */}
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-
-          <div>
-            <img
-              src={
-                movie1.Poster !== "N/A"
-                  ? movie1.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie1.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
+      {/* Display 10 movies*/}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-      </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
